697f77a6ccb4aafebddb23de18a7108e091d0867
Tobias Pfeiffer <pragtob@gmail.com>
2016-11-28 22:05:05 +0100

    Useful dev tools taken over

--- mix.exs
+++ mix.exs
@@ -34,8 +34,12 @@ defmodule BencheeHTML.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:benchee,      "~> 0.6", git: "git@github.com:PragTob/benchee.git"},
-      {:benchee_json, "~> 0.1", git: "git@github.com:PragTob/benchee_json.git"}
+      {:benchee,        "~> 0.6", git: "git@github.com:PragTob/benchee.git"},
+      {:benchee_json,   "~> 0.1", git: "git@github.com:PragTob/benchee_json.git"},
+      {:mix_test_watch, "~> 0.2",   only: :dev},
+      {:credo,          "~> 0.4",   only: :dev},
+      {:ex_doc,         "~> 0.11",  only: :dev},
+      {:earmark,        "~> 1.0.1", only: :dev}
     ]
   end
 
