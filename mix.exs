918ac0f3410013340f021db4427fcabb530efff3
Tobias Pfeiffer <pragtob@gmail.com>
2017-05-07 15:17:13 +0200

    dependency upgrades

--- mix.exs
+++ mix.exs
@@ -44,8 +44,8 @@ defmodule BencheeHTML.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:benchee,        "~> 0.7",   github: "PragTob/benchee", override: true},
-      {:benchee_json,   "~> 0.2",   github: "PragTob/benchee_json"},
+      {:benchee,        "~> 0.8"},
+      {:benchee_json,   "~> 0.3"},
       {:excoveralls,    "~> 0.6.1", only: :test},
       {:mix_test_watch, "~> 0.2",   only: :dev},
       {:credo,          "~> 0.4",   only: :dev},
