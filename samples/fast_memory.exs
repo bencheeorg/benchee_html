425a7cd4b7288b997f06b4933ac203db0a7f1a5f
Tobias Pfeiffer <pragtob@gmail.com>
2019-01-12 17:21:00 +0100

    fast sample to test memory usage with

---
+++ samples/fast_memory.exs
@@ -0,0 +1,16 @@
+list = Enum.to_list(1..10_000)
+map_fun = fn(i) -> [i, i * i] end
+
+Benchee.run(%{
+  "flat_map"    => fn -> Enum.flat_map(list, map_fun) end,
+  "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten end
+},
+  formatters: [
+    Benchee.Formatters.HTML,
+    Benchee.Formatters.Console
+  ],
+  formatter_options: [html: [file: "samples_output/my.html"]],
+  warmup: 0,
+  time: 0.5,
+  memory_time: 0.1
+)
