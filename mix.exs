8826d6e039c29275a50d33c95d2de215b9c8e933
Tobias Pfeiffer <pragtob@gmail.com>
2019-02-10 19:34:03 +0100

    Updates (#51)
    * hmm upgrades
    
    * fixup samples

--- mix.exs
+++ mix.exs
@@ -47,9 +47,9 @@ defmodule BencheeHTML.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:benchee, github: "PragTob/benchee", branch: "scenario-data-processed", override: true},
+      {:benchee, github: "PragTob/benchee", override: true},
       {:benchee_json, github: "PragTob/benchee_json"},
-      {:excoveralls, "~> 0.8.1", only: :test},
+      {:excoveralls, "~> 0.10.0", only: :test},
       {:mix_test_watch, "~> 0.2", only: :dev},
       {:credo, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
