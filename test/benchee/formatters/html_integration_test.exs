593d776b68fc97277b57175e7a82ce6497385b5c
Tobias Pfeiffer <pragtob@gmail.com>
2023-12-09 17:01:25 +0100

    make the entwinement of code slightly better

--- test/benchee/formatters/html_integration_test.exs
+++ test/benchee/formatters/html_integration_test.exs
@@ -4,12 +4,12 @@ defmodule Benchee.Formatters.HTMLIntegrationTest do
 
   @test_directory "test_output"
   @base_name "my"
-  @file_path "#{@test_directory}/#{@base_name}.html"
+  @index_path "#{@test_directory}/#{@base_name}.html"
   @comparison_path "#{@test_directory}/#{@base_name}_comparison.html"
 
   @default_test_directory "benchmarks/output"
   @default_base_name "results"
-  @default_file_path "#{@default_test_directory}/#{@default_base_name}.html"
+  @default_index_path "#{@default_test_directory}/#{@default_base_name}.html"
   @default_comparison_path "#{@default_test_directory}/#{@default_base_name}_comparison.html"
 
   test "works just fine" do
@@ -17,13 +17,13 @@ defmodule Benchee.Formatters.HTMLIntegrationTest do
       time: 0.01,
       memory_time: 0.01,
       warmup: 0.02,
-      formatters: [{Benchee.Formatters.HTML, file: @file_path, auto_open: false}]
+      formatters: [{Benchee.Formatters.HTML, file: @index_path, auto_open: false}]
     ]
 
     assertion_data = %{
       comparison_path: @comparison_path,
       test_directory: @test_directory,
-      file_path: @file_path,
+      index_path: @index_path,
       base_name: @base_name
     }
 
@@ -41,7 +41,7 @@ defmodule Benchee.Formatters.HTMLIntegrationTest do
     assertion_data = %{
       comparison_path: @default_comparison_path,
       test_directory: @default_test_directory,
-      file_path: @default_file_path,
+      index_path: @default_index_path,
       base_name: @default_base_name
     }
 
@@ -53,13 +53,13 @@ defmodule Benchee.Formatters.HTMLIntegrationTest do
       time: 0.01,
       memory_time: 0,
       warmup: 0.02,
-      formatters: [{Benchee.Formatters.HTML, file: @file_path, auto_open: false}]
+      formatters: [{Benchee.Formatters.HTML, file: @index_path, auto_open: false}]
     ]
 
     assertion_data = %{
       comparison_path: @comparison_path,
       test_directory: @test_directory,
-      file_path: @file_path,
+      index_path: @index_path,
       base_name: @base_name
     }
 
@@ -71,17 +71,39 @@ defmodule Benchee.Formatters.HTMLIntegrationTest do
       time: 0,
       memory_time: 0.01,
       warmup: 0.02,
-      formatters: [{Benchee.Formatters.HTML, file: @file_path, auto_open: false}]
+      formatters: [{Benchee.Formatters.HTML, file: @index_path, auto_open: false}]
     ]
 
     assertion_data = %{
       comparison_path: @comparison_path,
       test_directory: @test_directory,
-      file_path: @file_path,
+      index_path: @index_path,
       base_name: @base_name
     }
 
     basic_test(benchee_options, assertion_data, run_time: false)
+  end
+
+  test "works just fine using sequential_output" do
+    benchee_options = [
+      time: 0.01,
+      memory_time: 0.01,
+      warmup: 0.02,
+      formatters: [
+        fn suite ->
+          Benchee.Formatters.HTML.sequential_output(suite, %{file: @index_path, auto_open: false})
+        end
+      ]
+    ]
+
+    assertion_data = %{
+      comparison_path: @comparison_path,
+      test_directory: @test_directory,
+      index_path: @index_path,
+      base_name: @base_name
+    }
+
+    basic_test(benchee_options, assertion_data, run_time: true)
   end
 
   test "doesn't crash if we're essentially measuring nothing" do
@@ -109,29 +131,31 @@ defmodule Benchee.Formatters.HTMLIntegrationTest do
       )
 
       assert File.exists?(assertion_data.comparison_path)
+      sleep_path = "#{assertion_data.test_directory}/#{assertion_data.base_name}_sleep.html"
 
-      assert File.exists?(
-               "#{assertion_data.test_directory}/#{assertion_data.base_name}_sleep.html"
-             )
+      assert File.exists?(sleep_path)
 
-      assert File.exists?(
-               "#{assertion_data.test_directory}/#{assertion_data.base_name}_list.html"
-             )
+      list_path = "#{assertion_data.test_directory}/#{assertion_data.base_name}_list.html"
+      assert File.exists?(list_path)
 
-      assert File.exists?(assertion_data.file_path)
-      html = File.read!(assertion_data.comparison_path)
+      assert File.exists?(assertion_data.index_path)
+      comparison_html = File.read!(assertion_data.comparison_path)
 
-      assert html =~ "<body>"
-      assert html =~ "Sleep"
-      assert html =~ "List"
+      assert comparison_html =~ "<body>"
+      assert comparison_html =~ "Sleep"
+      assert comparison_html =~ "List"
 
       if Keyword.get(options, :run_time, false) do
-        assert html =~ "ips-comparison"
+        assert comparison_html =~ "ips-comparison"
       end
 
-      assert html =~ "System info</a>"
-      assert html =~ "benchee version"
-      assert html =~ "benchee_html version"
+      assert comparison_html =~ "System info</a>"
+      assert comparison_html =~ "benchee version"
+      assert comparison_html =~ "benchee_html version"
+
+      index_html = File.read!(assertion_data.index_path)
+      assert index_html =~ ~r/href="#{Path.basename(sleep_path)}"/
+      assert index_html =~ ~r/href="#{Path.basename(list_path)}"/
     end)
   after
     if File.exists?(assertion_data.test_directory), do: File.rm_rf!(assertion_data.test_directory)
